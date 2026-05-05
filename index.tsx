9359be53943ff5277afe768205cd667d73c7fb0e
Dyosercunha <dyoser2@gmail.com>
2026-05-05 00:08:14 -0300

    Harden React entrypoint

--- index.tsx
+++ index.tsx
@@ -2,8 +2,12 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 
-createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const container = document.getElementById("root");
+
+if (container) {
+  createRoot(container).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}

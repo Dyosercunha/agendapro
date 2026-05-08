607306756782b5a01995ecd7029913e46d2ebdfb
Dyosercunha <dyoser2@gmail.com>
2026-05-08 01:24:04 -0300

    Use App integrated pro features only

--- index.tsx
+++ index.tsx
@@ -3,17 +3,12 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import PlatformDashboard from "./PlatformDashboard";
-import SafeBackplatePanel from "./SafeBackplatePanel";
-import SafeServiceDelete from "./SafeServiceDelete";
-import SafeGrowthPanel from "./SafeGrowthPanel";
 
 const container = document.getElementById("root");
 const path = window.location.pathname.toLowerCase();
 const isPlatformRoute = window.location.search.includes("platform=1") || path.includes("/plataforma") || path.includes("/painel-plataforma");
 
 if (container) {
-  const app = isPlatformRoute
-    ? React.createElement(PlatformDashboard)
-    : React.createElement(React.Fragment, null, React.createElement(App), React.createElement(SafeBackplatePanel), React.createElement(SafeServiceDelete), React.createElement(SafeGrowthPanel));
+  const app = isPlatformRoute ? React.createElement(PlatformDashboard) : React.createElement(App);
   createRoot(container).render(React.createElement(React.StrictMode, null, app));
 }
